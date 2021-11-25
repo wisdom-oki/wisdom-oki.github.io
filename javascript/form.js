7e28754bc66eab13b9dd34f128bc840b4810b722
wiz <wisdomokiosifo@gmail.com>
2021-11-25 13:31:02 +0000

    added introduction to forms page

--- javascript/form.js
+++ javascript/form.js
@@ -1,1 +1,4 @@
 //form javascript
+	var name // the page will now ask for your name before filling more info
+		name = prompt (" Welcome. Please enter your name");
+		alert("Hi " + name+ "\n This page is to sign up for the next open day happening here. please fill in the valid information in the form"); 
