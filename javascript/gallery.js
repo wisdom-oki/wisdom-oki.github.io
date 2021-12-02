b9eb4d66d5085d972e6de3cd8f4844507ecc4fd2
18EOR <42158152+18EOR@users.noreply.github.com>
2021-12-02 21:34:55 +0000

    redone gallery style

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,22 +1,25 @@
-//Array for images
-const imagesArray = document.getElementsByClassName('gallery-image');
-//Array for the image text
-const textArray = document.getElementsByClassName('info-wrapper');
-//Array for the image buttons image-button
-const buttonArray = document.getElementsByClassName('image-button');
-console.log(buttonArray);
+// //Array for images
+// const imagesArray = document.getElementsByClassName('gallery-image');
+// //Array for the image text
+// const textArray = document.getElementsByClassName('info-wrapper');
+// //Array for the image buttons image-button
+// const buttonArray = document.getElementsByClassName('image-button');
 
-function galleryButton(id) {
-  //Hide all non-active iamges
-  for (let i = 0; i < imagesArray.length; i++) {
-    if (i != id) {
-      imagesArray[i].classList.add('image-hidden');
-      textArray[i].classList.add('info-hidden');
-      buttonArray[i].classList.remove('button-active');
-    } else {
-      imagesArray[i].classList.remove('image-hidden');
-      textArray[i].classList.remove('info-hidden');
-      buttonArray[i].classList.add('button-active');
-    }
-  }
-}
+// function galleryButton(id) {
+//   //Hide all non-active iamges
+//   for (let i = 0; i < imagesArray.length; i++) {
+//     if (i != id) {
+//       //hide non-active images/text
+//       imagesArray[i].classList.add('image-hidden');
+//       textArray[i].classList.add('info-hidden');
+//       //Un-highlight button
+//       buttonArray[i].classList.remove('button-active');
+//     } else {
+//       //show active image/text
+//       imagesArray[i].classList.remove('image-hidden');
+//       textArray[i].classList.remove('info-hidden');
+//       //highlight button
+//       buttonArray[i].classList.add('button-active');
+//     }
+//   }
+// }
