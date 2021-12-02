a2a9f4a7319243602d15e37f0007bdeb22cfa695
18EOR <42158152+18EOR@users.noreply.github.com>
2021-12-02 13:45:30 +0000

    optimised js for gallery

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -11,23 +11,11 @@ function galleryButton(id) {
   for (let i = 0; i < imagesArray.length; i++) {
     if (i != id) {
       imagesArray[i].classList.add('image-hidden');
-    } else {
-      imagesArray[i].classList.remove('image-hidden');
-    }
-  }
-  //Hide all non-active text
-  for (let i = 0; i < textArray.length; i++) {
-    if (i != id) {
       textArray[i].classList.add('info-hidden');
-    } else {
-      textArray[i].classList.remove('info-hidden');
-    }
-  }
-  //Hide all non-active buttons
-  for (let i = 0; i < textArray.length; i++) {
-    if (i != id) {
       buttonArray[i].classList.remove('button-active');
     } else {
+      imagesArray[i].classList.remove('image-hidden');
+      textArray[i].classList.remove('info-hidden');
       buttonArray[i].classList.add('button-active');
     }
   }
